9a6440e128dcc9248f86cfcd160acf752051e4cd
egozhuk <egorvzhukov@gmail.com>
2023-12-03 12:21:59 +0300

    first

--- ITMO_DZ_4/ThreadSafeArray.swift
+++ ITMO_DZ_4/ThreadSafeArray.swift
@@ -2,6 +2,7 @@ import Foundation
 
 class ThreadSafeArray<T>  {
     private var array: [T] = []
+    private let lock = NSRecursiveLock()
 }
 
 extension ThreadSafeArray: RandomAccessCollection {
@@ -12,10 +13,87 @@ extension ThreadSafeArray: RandomAccessCollection {
     var endIndex: Index { return array.endIndex }
 
     subscript(index: Index) -> Element {
-        get { return array[index] }
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return array[index]
+        }
+        
+        set {
+            lock.lock()
+            defer { lock.unlock() }
+            array[index] = newValue
+        }
+    }
+    
+    func append(_ value: T) {
+        lock.lock()
+        defer { lock.unlock() }
+        array.append(value)
+    }
+    
+    func toString() -> String {
+        lock.lock()
+        defer {lock.unlock() }
+        return "\(array)"
+    }
+    
+    func set(_ value: T, index: Int) {
+        lock.lock()
+        defer { lock.unlock() }
+        self[index] = value
+    }
+    
+    func dropFirst(_ n: Int = 1) -> [Element] {
+        lock.lock()
+        defer { lock.unlock() }
+        return Array(array.dropFirst(n))
     }
 
     func index(after i: Index) -> Index {
         return array.index(after: i)
     }
+    
+    func remove(at index: Index) {
+        lock.lock()
+        defer { lock.unlock() }
+        array.remove(at: index)
+    }
+
+    func insert(_ element: Element, at index: Index) {
+        lock.lock()
+        defer { lock.unlock() }
+        array.insert(element, at: index)
+    }
+
+    func removeAll() {
+        lock.lock()
+        defer { lock.unlock() }
+        array.removeAll()
+    }
+
+    var count: Int {
+        lock.lock()
+        defer { lock.unlock() }
+        return array.count
+    }
+
+    var isEmpty: Bool {
+        lock.lock()
+        defer { lock.unlock() }
+        return array.isEmpty
+    }
+
+    var first: Element? {
+        lock.lock()
+        defer { lock.unlock() }
+        return array.first
+    }
+
+    var last: Element? {
+        lock.lock()
+        defer { lock.unlock() }
+        return array.last
+    }
 }
+
--- ITMO_DZ_4/main.swift
+++ ITMO_DZ_4/main.swift
@@ -1,9 +1,30 @@
 import Foundation
 
-let ff = ThreadSafeArray<Int>()
-var dd = [1]
-dd.append(1)
-ff.dropFirst()
+//let ff = ThreadSafeArray<Int>()
+//var dd = [1]
+//dd.append(1)
+//ff.dropFirst()
 
-let sd = ff[0]
+//let sd = ff[0]
 
+//for index in 0..<100 {
+//    ff.append(index)
+//}
+//
+//print(ff.dropFirst(70))
+
+let taskA = BasicTask(qos: .userInitiated)
+let taskB = BasicTask(qos: .utility)
+let taskC = BasicTask(qos: .background)
+
+taskA.addDependency(taskB)
+taskA.addDependency(taskC)
+
+let taskManager = TaskManager()
+taskManager.add(taskA)
+taskManager.add(taskB)
+taskManager.add(taskC)
+
+taskManager.run()
+
+RunLoop.current.run()
